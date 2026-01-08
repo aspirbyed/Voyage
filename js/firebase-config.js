15989bcea36bbc6b6226fee5cf3a161d4dc57b0f
Edwin Thomas <142146998+aspirbyed@users.noreply.github.com>
2026-01-08 11:10:14 +0530

    Remove sensitive Firebase configuration details
    Updated firebaseConfig to an empty object for security.

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -3,15 +3,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.1/fireba
 import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-firestore.js";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyD8_qzK8wtX29bywPdu9vOVxXap_7EtbcQ",
-  authDomain: "voyage-towards-xmas.firebaseapp.com",
-  projectId: "voyage-towards-xmas",
-  storageBucket: "voyage-towards-xmas.firebasestorage.app",
-  messagingSenderId: "876014185552",
-  appId: "1:876014185552:web:60b4eabd048c3d2f2eb555",
-  measurementId: "G-CQJG905GEE"
-};
+const firebaseConfig = {}; // REPLACE with your firebaseConfig secret
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
